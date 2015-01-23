07d2390185d4e00ba504797d75dbb48d1ba3e166
Oliver Thauerer <up698897@myport.ac.uk>
2015-01-23 22:38:39 +0000

    Addded Time Script
    Did basic/first run of speach bubble script

---
+++ Anarchy/Assets/scripts/Speach.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Speach : MonoBehaviour {
+	public float ExistTime; 
+
+	float Time1;
+	float Time2;
+	// Use this for initialization
+	void Start () {
+		Time1 = Time.time;
+		Time2 = Time.time + ExistTime;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Time1 = Time.time;
+
+		if(Time1 >= Time2)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
