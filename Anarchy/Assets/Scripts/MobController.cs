132c8f80af79ed8680e4bc3df8f23b56519fa4f5
Alex Carter <alycarter@hotmail.co.uk>
2015-01-24 16:48:37 +0000

    oppes

--- Anarchy/Assets/Scripts/MobController.cs
+++ Anarchy/Assets/Scripts/MobController.cs
@@ -15,12 +15,12 @@ public class MobController : MonoBehaviour {
 	void Update () {
 		Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
 		direction.Normalize();
-		rigidbody.AddForce(direction * Time.deltaTime * 100);
+		rigidbody.AddForce(direction * Time.deltaTime * 200);
 		direction  = new Vector3(Input.GetAxis("FireX"),0,Input.GetAxis("FireY")).normalized;
 		if(fireDelay < 0 && direction.magnitude > 0.0f ){
 			ProjectileConrtoller PC = (ProjectileConrtoller)GameObject.Instantiate(projectile);
 			PC.owner = this.gameObject;
-			PC.Direction = direction*10.0f;
+			PC.Direction = direction*30.0f;
 			PC.Direction += rigidbody.velocity;
 			PC.transform.Translate(this.transform.position + (direction * ((BoxCollider)collider).size.x));
 			PC.transform.LookAt(PC.transform.position + PC.Direction, Vector3.up);
